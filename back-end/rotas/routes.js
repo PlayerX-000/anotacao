577320e291f222a179e3be4b4aeffd20116744e1
PlayerX-000 <kauan.mghenrique@gmail.com>
2021-12-04 15:40:01 -0300

    Progeto Atual

--- back-end/rotas/routes.js
+++ back-end/rotas/routes.js
@@ -5,11 +5,6 @@ const busca_dados = require("../../controlador/controlador");
 
 router.get("/", busca_dados.Inicial)
 
-router.get("/inicio", busca_dados.Inicial)
-
-router.get("/inicio/page2", busca_dados.Page2)
-
-router.get("/inicio/page2/page3", busca_dados.Page3)
 
 /***************************************/
 /**************** POST *****************/
--- controlador/controlador.js
+++ controlador/controlador.js
@@ -1,11 +1,3 @@
 exports.Inicial = ((req, res)=>{
     res.render("./index",{});
 })
-
-exports.Page2 = ((req, res)=>{
-    res.render("./page2",{});
-})
-
-exports.Page3 = ((req, res)=>{
-    res.render("./page3",{});
-})
