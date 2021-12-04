8a694fbdc39b2e6e43dd4e289044ba269d62a12f
PlayerX-000 <kauan.mghenrique@gmail.com>
2021-12-04 15:57:46 -0300

    update

--- back-end/rotas/routes.js
+++ back-end/rotas/routes.js
@@ -5,10 +5,6 @@ const busca_dados = require("../../controlador/controlador");
 
 router.get("/", busca_dados.Inicial)
 
-router.get("/inicio", busca_dados.Inicial)
-
-router.get("/inicio/page2", busca_dados.Page2)
-
 /***************************************/
 /**************** POST *****************/
 
--- controlador/controlador.js
+++ controlador/controlador.js
@@ -2,6 +2,3 @@ exports.Inicial = ((req, res)=>{
     res.render("./index",{});
 })
 
-exports.Page2 = ((req, res)=>{
-    res.render("./page2",{});
-})
