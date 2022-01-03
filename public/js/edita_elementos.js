6423c50806a63699cc75ee197af56db56c68bdc8
PlayerX-000 <kauan.mghenrique@gmail.com>
2022-01-03 17:34:16 -0300

    update

--- public/js/edita_elementos.js
+++ public/js/edita_elementos.js
@@ -1,5 +1,7 @@
+const elemento = document.getElementById('conteudo');
 function autoResize(el){
-    while (el.scrollHeight > el.offsetHeight){
-      el.rows += 1;
-    }
+  while (el.scrollHeight > el.offsetHeight){
+    el.rows += 1;
   }
+}
+autoResize(elemento);
